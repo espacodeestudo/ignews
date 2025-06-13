f6540cffdab2e3570f7369e131034fa242bd4277
yuransimao <yuransimao7@gmail.com>
2025-06-13 12:46:35 +0100

    added button Subscribe

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import styled from "./styles.module.scss"
+
+function SubscribeButton() {
+  return (
+    <button type="button" className={styled.subscribeButton}>
+        Subscribe now
+    </button>
+  )
+}
+
+export {SubscribeButton}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import styled from "./home.module.scss"
+import {SubscribeButton} from "../components/SubscribeButton"
 export default function Home() {
   return (
     <>
@@ -17,6 +18,7 @@ export default function Home() {
             <span>for $9.90 month</span>
           
           </p>
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
